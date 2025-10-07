cba47d0b75b843b8190e8f3e2d8efe271da38dab
ludmilaprolygin <mlprolygin@gmail.com>
2025-10-06 22:20:50 -0300

    set for Etapa3

--- resources/sinErrores/correccion01.java
+++ resources/sinErrores/correccion01.java
@@ -1,6 +1,9 @@
 ///[SinErrores]
 
 class A {
+    Id1.m1(4).m2(5, this, 'c') = 5; //Debe andar para futuras etapas!!!
+    A.b().a = 5;
+
     void m1() {
         Id1.m1(4).m2(5, this, 'c') = 5; //Debe andar para futuras etapas!!!
         A.b().a = 5;
