17cd7644ccf7c30dfbdd8500825c9956133b7fe0
ludmilaprolygin <mlprolygin@gmail.com>
2025-11-16 16:39:44 -0300

    checkpoint

--- src/model/AST/Encadenados/NodoLLamadaEncadenada.java
+++ src/model/AST/Encadenados/NodoLLamadaEncadenada.java
@@ -72,40 +72,60 @@ public class NodoLLamadaEncadenada extends Encadenado {
 
     @Override
     public void gen(OutputManager o, AbstractType tipo) {
-        for(NodoExpresion n : parametros){
-            n.gen(o);
-            //o.gen(Instructions.SWAP.toString());
+
+        // 1. GENERAR PARAMETROS (sin swap)
+        for (NodoExpresion n : parametros) {
+            n.gen(o);                  // deja p1, luego p2, … sobre la pila
         }
 
-        if(!associatedMethod.getReturnType().getName().getLexeme().equals(reservedVoid.getTypeExplanation())){
+        // 2. SI EL METODO NO ES VOID, DUPLICAR RESULTADO DEL CALL
+        boolean retornaValor =
+                !associatedMethod.getReturnType().getName().getLexeme()
+                        .equals(reservedVoid.getTypeExplanation());
+
+        // 3. RESOLVER ESTATICO VS DINAMICO
+        if (associatedMethod.getModifier() != null &&
+                associatedMethod.getModifier().getLexeme()
+                        .equals(reservedStatic.getTypeExplanation())) {
+
+            // método estático: sacar el this que está debajo de los parámetros
+            // quitar un único elemento this → POP
+            o.gen(Instructions.POP.toString());
+
+            // cargar la etiqueta del método estático
+            o.gen(Instructions.PUSH + " lbl_"
+                    + associatedMethod.getName().getLexeme()
+                    + "@" + associatedMethod.getCreator().getName().getLexeme());
+        }
+        else {
+            // ---- MÉTODO DINÁMICO ----
+            // aquí en la pila tenemos: retAddr … this p1 … pn
+
+            // obtener la VT del objeto (this está en el tope de la pila RECIÉN
+            // después de pasar los parámetros)
+            o.gen(Instructions.LOADREF + " 0");
+
+            // apuntar a la VT correcta según tipo
+            o.gen(Instructions.PUSH + " VT@" + tipo.getName().getLexeme());
+
+            // obtener el método en la VT (offset ya resuelto del método)
+            o.gen(Instructions.LOADREF + " " + associatedMethod.getOffset());
+        }
+
+        // 4. HACER EL CALL
+        o.gen(Instructions.CALL.toString());
+
+        // 5. SI NO ES VOID, DUPLICAR EL RESULTADO PARA CADENAR
+        if (retornaValor) {
             o.gen(Instructions.DUP.toString());
         }
 
-        if(associatedMethod.getModifier() != null && associatedMethod.getModifier().getLexeme().equals(reservedStatic.getTypeExplanation())){
-            o.gen(Instructions.POP.toString());
-            o.gen(Instructions.PUSH + " lbl_" + associatedMethod.getName().getLexeme() + "@" + associatedMethod.getCreator().getName().getLexeme());
-        }
-        else{
-            System.out.println("gen en NodoLLamadaEncadenada: ENTRO AL ELSE");
-            //o.gen(Instructions.LOAD + " " + CodeGenConfig.OFFSET_THIS);
-            //o.gen(Instructions.DUP.toString());
-            o.printStackTop();
-            o.gen(Instructions.LOADREF + " 0");
-            o.printStackTop();
-            o.gen(Instructions.PUSH + " VT@" + tipo.getName().getLexeme());
-            o.printStackTop();
-            o.gen(Instructions.LOADREF + " " + associatedMethod.getOffset());
-            //o.printStackTop();
-        }
-
-        o.printStackTop();
-
-        o.gen(Instructions.CALL.toString());
-
-        if(encadenado != null && !(encadenado instanceof EncadenadoVacio)){
+        // 6. SI HAY ENCADENADO, GENERARLO
+        if (encadenado != null && !(encadenado instanceof EncadenadoVacio)) {
             encadenado.gen(o, associatedMethod.getReturnType());
         }
     }
+
 
     private void compareArgs(Method m) throws SemanticException {
         if (m.getParameters().size() != parametros.size())
