16e522321c03519da421acada263c5b05e41f365
ludmilaprolygin <mlprolygin@gmail.com>
2025-11-20 22:30:39 -0300

    checkpoint

---
+++ resources/sinErrores/sinError_25.java
@@ -0,0 +1,16 @@
+///1234&0&exitosamente
+
+class PorFavorFunciona {
+    static void main(){
+        var v1 = 0;
+        if (v1 == 0) {
+            var v2 = 5;
+            m1();
+        }
+        debugPrint(v1);
+    }
+
+    static void m1(){
+        debugPrint(1234);
+    }
+}
