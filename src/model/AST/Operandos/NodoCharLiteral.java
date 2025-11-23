583b6e57527893d550f3155a0a7378ef2b083003
ludmilaprolygin <mlprolygin@gmail.com>
2025-11-23 11:17:00 -0300

    cracateres de escape

--- src/model/AST/Operandos/NodoCharLiteral.java
+++ src/model/AST/Operandos/NodoCharLiteral.java
@@ -20,7 +20,33 @@ public class NodoCharLiteral extends NodoOperando {
 
     @Override
     public void gen(OutputManager o) {
-        o.gen(Instructions.PUSH + " " + token.getLexeme());
+        int prepToken = (int) token.getLexeme().charAt(1); // Obtener el valor ASCII del carácter
+        if (token.getLexeme().charAt(1) == '\\') { // Manejar caracteres de escape
+            switch (token.getLexeme().charAt(2)) {
+                case 'n':
+                    prepToken = 10; // Nueva línea
+                    break;
+                case 't':
+                    prepToken = 9; // Tabulación
+                    break;
+                case 'r':
+                    prepToken = 13; // Retorno de carro
+                    break;
+                case '\'':
+                    prepToken = 39; // Comilla simple
+                    break;
+                case '\"':
+                    prepToken = 34; // Comilla doble
+                    break;
+                case '\\':
+                    prepToken = 92; // Barra invertida
+                    break;
+                default:
+                    prepToken = (int) token.getLexeme().charAt(2); // Otros caracteres de escape
+                    break;
+            }
+        }
+        o.gen(Instructions.PUSH + " " + prepToken);
     }
 }
 
