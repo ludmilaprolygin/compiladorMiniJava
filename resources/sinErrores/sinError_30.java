678031cadb044190a853695282dcd31e981bd152
ludmilaprolygin <mlprolygin@gmail.com>
2025-11-24 20:15:41 -0300

    checkpoint

---
+++ resources/sinErrores/sinError_30.java
@@ -0,0 +1,16 @@
+///50&10&A&false&exitosamente
+
+class Init {
+    static void main() {
+        var v = 50;
+        System.printIln(v);
+
+        int i = 10;
+        char c = 'A';
+        boolean b = false;
+
+        System.printIln(i);
+        System.printCln(c);
+        System.printBln(b);
+    }
+}
